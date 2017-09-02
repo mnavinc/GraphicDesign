ce91e0d96f4c4d419d4df7fae0401b595e3b4515
mnavinc <naveen@tententen.in>
2017-09-02 15:31:39 +0530

    added files - started integrating

---
+++ static/main.js
@@ -0,0 +1,8 @@
+// $.getJSON('static/test.json', function(data) {
+//     $.each(data, function (inData) {
+//         $.each(inData, function () {
+//             $(".box.task").append('<div class="myThing">' + inData.subtopic_id + '</div>'); 
+//             $(".box.").append('<div class="myThing2">' + inData.subtopic_name + '</div>');  
+//         });             
+//     });
+// });
